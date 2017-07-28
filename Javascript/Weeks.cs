cbe864a1d610af314a3e16a7eef7b99842f99afd
Bahman Masarratbakhsh <atropatnet@yahoo.com>
2017-07-28 04:23:20 -0700

    Add project files.

---
+++ Javascript/Weeks.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Javascript
+{
+    public class StoreTimes
+    {
+        public DayOfWeek Day { get; set; }
+        public string StartTime { get; set; }
+        public string EndTime { get; set; }
+    }
+}
